de1b578847f309fd9e59659d2ef5f492371ed819
Bandawe <bands500@gmail.com>
2024-06-10 13:08:41 +0200

    changed DB_USERNAME parameter in env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -26,7 +26,7 @@ import * as path from 'path';
       type: 'postgres',
       host: process.env.DB_HOST,
       port: parseInt(process.env.DB_PORT),
-      username: process.env.DB_USER,
+      username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
 
