a3ae768c49d7fb996f0bfe8525a55ebb756f97c0
Bandawe <bands500@gmail.com>
2024-06-10 13:28:56 +0200

    reverted to f99883b

--- src/app.module.ts
+++ src/app.module.ts
@@ -29,18 +29,8 @@ import * as path from 'path';
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
-
-      /* --------for local connections only---------- */
-      // type: 'mysql',
-      // host: 'localhost',
-      // port: 3306,
-      // username: 'root',
-      // password: 'root',
-      // database: 'school',
-
-      /* --------End local connection----------------*/
       autoLoadEntities: true,
-      synchronize: true,
+      synchronize: false,
     }),
     ResourceByIdModule,
     EnrolmentModule,
